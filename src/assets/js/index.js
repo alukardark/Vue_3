41a8295276387beee752c35949f5b23695408c2d
Андрей Рябченко <ryabchenko.a@delcomp.ru>
2021-03-24 12:17:52 +0300

    'x'

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -56,12 +56,33 @@ const App = {
             placeholderString: 'Введите название заметки',
             title: 'Список заметок',
             inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2']
+            notes: ['Заметка 1', 'Заметка 2'],
         }
     },
     methods: {
-        inputChangeHandler(event){
-            this.inputValue = event.target.value;
+        addNewNote() {
+            if (this.inputValue != '') {
+                this.notes.push(this.inputValue);
+                this.inputValue = '';
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase();
+        },
+        removeNote(index) {
+            this.notes.splice(index, 1);
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            return this.notes.length * 2;
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = '';
+            }
         }
     }
 };
